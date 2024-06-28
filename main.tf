c2b6f7610497e6d78ff796f42c89484ee831ef96
praneeth-balamurugan <praneeth.nkl@gmail.com>
2024-06-28 23:39:59 +0530

    bucket handling

--- main.tf
+++ main.tf
@@ -4,15 +4,16 @@ provider "aws" {
 
 data "aws_s3_bucket" "existing_bucket" {
   bucket = "terraform-pre-098765"
+  count  = length([for b in try(aws_s3_bucket.existing_bucket.bucket, []) : b]) > 0 ? 1 : 0
 }
 
 resource "aws_s3_bucket" "mybucket" {
-  count  = data.aws_s3_bucket.existing_bucket.bucket != null ? 0 : 1
-  bucket = "terraform-final-09876"
+  count  = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? 1 : 0
+  bucket = "terraform-pre-098765"
 }
 
 resource "aws_s3_bucket_website_configuration" "mybucket_website" {
-  bucket = data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.mybucket[0].bucket
+  bucket = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].bucket : data.aws_s3_bucket.existing_bucket.bucket
 
   index_document {
     suffix = "index.html"
@@ -24,7 +25,7 @@ resource "aws_s3_bucket_website_configuration" "mybucket_website" {
 }
 
 resource "aws_s3_bucket_ownership_controls" "example" {
-  bucket = data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.mybucket[0].id
+  bucket = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].id : data.aws_s3_bucket.existing_bucket.id
 
   rule {
     object_ownership = "BucketOwnerPreferred"
@@ -32,7 +33,7 @@ resource "aws_s3_bucket_ownership_controls" "example" {
 }
 
 resource "aws_s3_bucket_public_access_block" "example" {
-  bucket = data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.mybucket[0].id
+  bucket = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].id : data.aws_s3_bucket.existing_bucket.id
 
   block_public_acls       = false
   block_public_policy     = false
@@ -46,12 +47,12 @@ resource "aws_s3_bucket_acl" "example" {
     aws_s3_bucket_public_access_block.example,
   ]
 
-  bucket = data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.mybucket[0].id
+  bucket = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].id : data.aws_s3_bucket.existing_bucket.id
   acl    = "public-read"
 }
 
 resource "aws_s3_bucket_policy" "mybucket_policy" {
-  bucket = data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.bucket : aws_s3_bucket.mybucket[0].id
+  bucket = length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].id : data.aws_s3_bucket.existing_bucket.id
 
   policy = jsonencode({
     Version = "2012-10-17"
@@ -60,7 +61,7 @@ resource "aws_s3_bucket_policy" "mybucket_policy" {
         Effect = "Allow"
         Principal = "*"
         Action = "s3:GetObject"
-        Resource = "${data.aws_s3_bucket.existing_bucket.bucket != null ? data.aws_s3_bucket.existing_bucket.arn : aws_s3_bucket.mybucket[0].arn}/*"
+        Resource = "${length(data.aws_s3_bucket.existing_bucket.id) == 0 ? aws_s3_bucket.mybucket[0].arn : data.aws_s3_bucket.existing_bucket.arn}/*"
       }
     ]
   })
